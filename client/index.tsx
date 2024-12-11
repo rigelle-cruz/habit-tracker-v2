0eb2e200c0460d191c6b0f16ee5ecb1997c4e909
Rigelle Cruz <rigellecruz@gmail.com>
2024-12-11 14:01:33 +1300

    Fix css file

---
+++ client/index.tsx
@@ -0,0 +1,26 @@
+import { createRoot } from 'react-dom/client';
+import {
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+} from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import App from './App';
+
+
+const routes = createRoutesFromElements(<Route path="/" element={<App />} />);
+
+export function AppProvider() {
+	return <RouterProvider router={createBrowserRouter(routes)} />;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	const queryClient = new QueryClient();
+	createRoot(document.getElementById('app') as HTMLElement).render(
+		<QueryClientProvider client={queryClient}>
+			<AppProvider />
+		</QueryClientProvider>
+	);
+});
