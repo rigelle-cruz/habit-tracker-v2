fbec3ba1ecb0bd35cda20c8cb3519387aa6ad87b
Rigelle Cruz <rigellecruz@gmail.com>
2024-12-13 12:02:30 +1300

    Make imports easier

---
+++ client/pages/index.ts
@@ -0,0 +1,12 @@
+import ErrorPage from './ErrorPage'
+import HabitList from './HabitList
+import DeletedHabits from './DeletedHabits'
+import PlantCollection from './PlantCollection'
+
+export {
+    ErrorPage,
+    HabitList,
+    DeletedHabits,
+    PlantCollection
+
+}
